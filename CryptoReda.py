520bd13e0b1789eedc384eec32aafeef42d5c67f
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-26 23:02:34 +0100

    Updates

--- CryptoReda.py
+++ CryptoReda.py
@@ -1,7 +1,22 @@
 import argparse
 import pprint
+import sys
 from typing import Optional, Sequence
 from Utilities import encryption, decryption
+
+def write_if_possible(args: dict):
+    try:
+        with args.output as file:
+            pprint.pprint(args['output'])
+    except:
+        pass
+
+def read_if_possible(args: dict):
+    try:
+        with args.file as file:
+            pprint.pprint(args['file'])
+    except:
+        pass
 
 def read(parser, args: dict):
     if args['input'] is not None and args['key'] is None:
@@ -9,22 +24,21 @@ def read(parser, args: dict):
 
     if args['decrypt'] and (args['key'] is None or args['input'] is None):
         parser.error("argument -d/--decrypt requires argument --key and argument --input.")
+
+    if args['input'] is not None and args['file'] is not None:
+        parser.error("argument -i/--input can't be with argument --file Choose one input argument.")
+
     elif args['decrypt'] and (args['key'] is not None and args['input'] is not None):
         pprint.pprint("Decryption")
         pprint.pprint(decryption.decrypt(args['input'], args['key']))
 
     if args['encrypt'] and (args['key'] is None or args['input'] is None):
         parser.error("argument -e/--encrypt requires argument --key and argument --input.")
+
     elif args['encrypt'] and (args['key'] is not None and args['input'] is not None):
         pprint.pprint("Encryption")
         pprint.pprint(encryption.encrypt(args['input'], args['key']))
     
-    try:
-        with args.file as file:
-            pprint.pprint(args['file'])
-    except:
-        pass
-        
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
@@ -34,15 +48,19 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument('-e', '--encrypt',help="Encrypt the input using the CryptoReda Algorithm.", action="store_true")
     parser.add_argument('-d', '--decrypt',help="Decrypt the input using the CryptoReda Algorithm.", action="store_true")
     parser.add_argument('-k', '--key',help="Specify the key String.")
-    parser.add_argument('-o', '--output',help="Specify the output file.")
+    parser.add_argument('-o', '--output',help="Specify the output file.", type=argparse.FileType('w'))
 
-    args   = parser.parse_args(argv)
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        return -1
+    else:
+        args   = parser.parse_args(argv)
 
-    read(parser ,vars(args))
+        read(parser, vars(args))
 
-    # pprint.pprint(vars(args))
+        # pprint.pprint(vars(args))
 
-    return 0
+        return 0
 
 if __name__ == '__main__':
     exit(main())
