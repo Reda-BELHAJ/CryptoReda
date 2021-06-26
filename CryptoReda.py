9dccb0afcc9470162b00742bde2a1abb2f8cc2e4
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-26 19:14:07 +0100

    Updates

--- CryptoReda.py
+++ CryptoReda.py
@@ -4,22 +4,24 @@ from typing import Optional, Sequence
 from Utilities import *
 
 def read(args: dict):
-    if args['input'] is not None:
-        pprint.pprint(args['input'])
-    if args['output'] is not None:
-        pprint.pprint(args['output'])
     if args['decrypt']:
         pprint.pprint(args['decrypt'])
+
     if args['encrypt']:
         pprint.pprint(args['encrypt'])
-    if args['file'] is not None:
-        pprint.pprint(args['file'])
+    
+    try:
+        with args.file as file:
+            pprint.pprint(args['file'])
+    except:
+        pass
+        
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-i', '--input',help="Specify the Input String.")
-    parser.add_argument('-f', '--file',help="Specify the Input File String.")
+    parser.add_argument('-f', '--file',help="Specify the Input File String.", type=argparse.FileType('r'))
     parser.add_argument('-e', '--encrypt',help="Encrypt the input using the CryptoReda Algorithm.", action="store_true")
     parser.add_argument('-d', '--decrypt',help="Decrypt the input using the CryptoReda Algorithm.", action="store_true")
     parser.add_argument('-o', '--output',help="Specify the output file.")
@@ -28,7 +30,7 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     read(vars(args))
 
-    # pprint.pprint(vars(args))
+    pprint.pprint(vars(args))
 
     return 0
 
