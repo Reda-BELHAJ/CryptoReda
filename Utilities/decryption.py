d29bc84f31b3a76a63028cfb309cc352e7b76aa0
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-26 21:41:48 +0100

    Updates

--- Utilities/decryption.py
+++ Utilities/decryption.py
@@ -1,10 +1,16 @@
 LIMITS_Upp = (65, 90)
 LIMITS_Dwn = (97, 122)
 
+def hasNumbers(inputStr):
+    return any(char.isdigit() for char in inputStr)
+
 def decrypt(input: str, key: str):
-    pass
+    if hasNumbers(key):
+        pass
+    else:
+        pass
 
 
-input = "Test"
+input = "tedasTscab"
 key = "1dad54"
 key2 = "dsa"
