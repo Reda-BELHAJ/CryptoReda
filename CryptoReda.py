fb79c1eae83746981847ec826dea5b314996df97
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-26 19:50:43 +0100

    Changes

--- CryptoReda.py
+++ CryptoReda.py
@@ -3,7 +3,10 @@ import pprint
 from typing import Optional, Sequence
 from Utilities import *
 
-def read(args: dict):
+def read(parser, args: dict):
+    if args['input'] is not None and args['key'] is None:
+        parser.error("argument -i/--input requires argument --key.")
+
     if args['decrypt']:
         pprint.pprint(args['decrypt'])
 
@@ -24,11 +27,12 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument('-f', '--file',help="Specify the Input File String.", type=argparse.FileType('r'))
     parser.add_argument('-e', '--encrypt',help="Encrypt the input using the CryptoReda Algorithm.", action="store_true")
     parser.add_argument('-d', '--decrypt',help="Decrypt the input using the CryptoReda Algorithm.", action="store_true")
+    parser.add_argument('-k', '--key',help="Specify the key String.")
     parser.add_argument('-o', '--output',help="Specify the output file.")
 
     args   = parser.parse_args(argv)
 
-    read(vars(args))
+    read(parser ,vars(args))
 
     pprint.pprint(vars(args))
 
---
+++ Utilities.py
@@ -0,0 +1,8 @@
+import math
+
+
+def encrypt(input: str, key: str):
+    pass
+
+def decrypt(input: str, key: str):
+    pass
