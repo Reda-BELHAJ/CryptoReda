9d2dae528426f8e8db61901abf70e7fa1e1b7853
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-26 21:00:20 +0100

    Updates

--- Utilities.py
+++ Utilities.py
@@ -1,20 +1,35 @@
 import math
+import random
 
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
+def getEO(inputString):
+    return inputString[::2], inputString[1::2]
+
 def getNumbers(inputString):
-    return [char for char in inputString if char.isdigit()]
+    numbers   = [char for char in inputString if char.isdigit()]
+    nums      = []
+
+    for i in range(len(inputString)):
+        nums.append(i)
+
+    numbers += nums
+
+    return numbers
 
 def encrypt(input: str, key: str):
     hash_str = ""
     input    =  input[::-1]
+    input   += key
+    even_str, odd_str  = getEO(input)
+    hash_str = even_str + odd_str
     
     if hasNumbers(key):
-        numbers   = getNumbers(key)
+        numbers = getNumbers(key)
+        print(numbers)
     else:
         pass
-
     return hash_str
 
 def decrypt(input: str, key: str):
@@ -22,6 +37,6 @@ def decrypt(input: str, key: str):
 
 
 input = "Test"
-key = "1dad5"
+key = "1dad54"
 
-# print(encrypt(input, key))
+print(encrypt(input, key))
