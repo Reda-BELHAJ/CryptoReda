02588e386256be536b6d50a9d59af4dbecf156e8
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-06-26 20:40:42 +0100

    Updates

--- Utilities.py
+++ Utilities.py
@@ -1,8 +1,27 @@
 import math
 
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def getNumbers(inputString):
+    return [char for char in inputString if char.isdigit()]
 
 def encrypt(input: str, key: str):
-    pass
+    hash_str = ""
+    input    =  input[::-1]
+    
+    if hasNumbers(key):
+        numbers   = getNumbers(key)
+    else:
+        pass
+
+    return hash_str
 
 def decrypt(input: str, key: str):
     pass
+
+
+input = "Test"
+key = "1dad5"
+
+# print(encrypt(input, key))
